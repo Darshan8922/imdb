6953a9705d16f6dbfe6f59b5a4d97c5fb40e4fb2
Shah Darshan <darshanshah8922@gmail.com>
2024-04-12 17:08:54 +0530

    deploy new

--- imdb_api/urls.py
+++ imdb_api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('list/<int:pk>', views.movie_detail, name='movie-detail'),
     path('stream', views.stream_list, name="stream-platform"),
     path('stream/<int:pk>', views.stream_detail, name="stream-platform-detail"),
-    path("", views.try, name="try"),
+    path("", views.try, name="try")
 ]
